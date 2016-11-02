c8dc8a7ee5ce38065f784aa066fb3f0968775e96
mikeadossi <mikeadossi@gmail.com>
2016-11-02 10:10:57 -0700

    previousOperator is functional

--- main.js
+++ main.js
@@ -1,89 +1,50 @@
 $(document).ready(function(){
 
-	$('#acBtn').click(function(){
-		document.getElementById('firstScreen').value = '';
-		document.getElementById('secondScreen').value = '';
-		console.log('yolo')
-	})
+	var operationStack = '';
+	var previousOperator = '';
+	var fred = { // bit fields
+		mult : 1,
+		divis : 2,
+		add : 4, 
+		subtr : 8,
+		number : 16,
+		ac : 32,
+		ce : 64
+	}
 
-	$('#one').click(function(){
-		document.getElementById('firstScreen').value += '1';
-		document.getElementById('secondScreen').value += '1';
-	})
-
-	$('#two').click(function(){
-		document.getElementById('firstScreen').value += '2';
-		document.getElementById('secondScreen').value += '2';
-	})
-
-	$('#three').click(function(){
-		document.getElementById('firstScreen').value += '3';
-		document.getElementById('secondScreen').value += '3';
-	})
-
-	$('#four').click(function(){
-		document.getElementById('firstScreen').value += '4';
-		document.getElementById('secondScreen').value += '4';
-	})
-
-	$('#five').click(function(){
-		document.getElementById('firstScreen').value += '5';
-		document.getElementById('secondScreen').value += '5';
-	})
-
-	$('#six').click(function(){
-		document.getElementById('firstScreen').value += '6';
-		document.getElementById('secondScreen').value += '6';
-	})
-
-	$('#seven').click(function(){
-		document.getElementById('firstScreen').value += '7';
-		document.getElementById('secondScreen').value += '7';
-	})
-
-	$('#eight').click(function(){
-		document.getElementById('firstScreen').value += '8';
-		document.getElementById('secondScreen').value += '8';
-	})
-
-	$('#nine').click(function(){
-		document.getElementById('firstScreen').value += '9';
-		document.getElementById('secondScreen').value += '9';
-	})
-
-	$('#zero').click(function(){
-		document.getElementById('firstScreen').value += '0';
-		document.getElementById('secondScreen').value += '0';
-	})
-
-	$('#plusOperator').click(function(){
-		document.getElementById('firstScreen').value += '+';
-		document.getElementById('secondScreen').value += '+';
-	})
-
-	$('#minusOperator').click(function(){
-		document.getElementById('firstScreen').value += '-';
-		document.getElementById('secondScreen').value += '-';
-	})
-
-	$('#multiplicationOperator').click(function(){
-		document.getElementById('firstScreen').value += 'x';
-		document.getElementById('secondScreen').value += 'x';
-	})
-
-	$('#divisionOperator').click(function(){
-		document.getElementById('firstScreen').value += '÷';
-		document.getElementById('secondScreen').value += '÷';
-	})
-
-	$('#point').click(function(){
-		document.getElementById('firstScreen').value += '.';
-		document.getElementById('secondScreen').value += '.';
-	})
-
-	$('#equalsBtn').click(function(){
-		var solution;
-		var problem = document.getElementById('secondScreen').value
-		document.getElementById().value += solution
+	var operator = $('.buttons').each(function(){
+		var that = $(this).html()
+		$(this).click(function(){
+			console.log($(this),'\n\n',that)
+			if(that == 'AC'){
+				previousOperator = fred.ac;
+				document.getElementById('firstScreen').value = '';
+				document.getElementById('secondScreen').value = '';
+			} else if (!isNaN(that)){ // is a number (double neg)
+				document.getElementById('firstScreen').value += that;
+				operationStack += that;
+				previousOperator = fred.number;
+			} else { // is a operator
+		        document.getElementById('firstScreen').value = that;
+		        document.getElementById('secondScreen').value += that;
+		        operationStack += that;
+		        if(that == '+'){ previousOperator = fred.add }else if(that == '-'){
+		        	previousOperator = fred.subtr
+		        }
+			}
+			console.log('\n',previousOperator)
+		})
 	})
 })
+
+
+
+
+
+
+
+
+
+
+
+
